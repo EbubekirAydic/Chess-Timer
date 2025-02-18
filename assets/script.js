5bdf9cdb3029209e50ac7604461509da03ee7fa9
Ebubekir Aydıç <ebuaydic2@gmail.com>
2025-02-18 16:19:00 +0300

    Daha düzgün yapıldı!

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-let whiteTime, blackTime, increment, currentTurn = null, interval;
+let whiteTime, blackTime, increment, currentTurn = null, Tester = false, interval;
         
 function startGame() {
     let baseMinutes = parseInt(document.getElementById("baseTime").value);
@@ -29,12 +29,34 @@ function switchTurn(player) {
 }
 
 function updateDisplay() {
-    document.getElementById("whiteTime").innerText = formatTime(whiteTime);
-    document.getElementById("blackTime").innerText = formatTime(blackTime);
+    document.getElementById("whiteTime").innerHTML = formatTime(whiteTime);
+    document.getElementById("blackTime").innerHTML = formatTime(blackTime);
 }
 
 function formatTime(seconds) {
     let minutes = Math.floor(seconds / 60);
     let secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+    return "<b>" + `${minutes}:${secs < 10 ? '0' : ''}${secs}` + "</b>";
+}
+
+if (/Mobi|Android/i.test(navigator.userAgent) || Tester) {
+    console.log("Mobil cihaz kullanılıyor.");
+
+    // 180 derece döndürme işlemi
+    document.getElementById("Timess").innerHTML = `
+                <div class="row my-1">
+                    <div class="col OrtalamaXY">
+                        <div class="timer" id="whiteTime">5:00</div>
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col OrtalamaXY">
+                        <div class="timer" id="blackTime">5:00</div>
+                    </div>
+                </div>`
+                
+    document.getElementById("whiteTime").style.transform = "rotate(180deg)";
+
+} else {
+    console.log("Masaüstü cihaz kullanılıyor.");
 }
